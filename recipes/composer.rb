08d76386cd3c25ce88a9b35c98a525f0058caf8d
hannesvdvreken <vandevreken.hannes@gmail.com>
2014-05-15 11:52:23 +0200

    Fallback to unzip for composer --prefer-dist

--- recipes/composer.rb
+++ recipes/composer.rb
@@ -5,11 +5,16 @@
 # Author:: Hannes Van De Vreken (<vandevreken.hannes@gmail.com>)
 #
 # MIT Licensed
-# 
+#
 
 # install composer if needed
 execute "composer-install" do
 	command "wget -O composer http://getcomposer.org/composer.phar && chmod +x composer"
 	creates "#{node['phptools']['install_path']}/composer"
 	cwd node['phptools']['install_path']
+end
+
+# Install unzip as a fallback for hhvm & composer for prefer-dist
+apt_package "unzip" do
+	action :install
 end
