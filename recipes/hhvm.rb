9d2649878bf58daf66d7a02a9b6612f2803294f2
hannesvdvreken <vandevreken.hannes@gmail.com>
2014-05-16 10:42:06 +0200

    Make curl requests not time out after 5 seconds

--- recipes/hhvm.rb
+++ recipes/hhvm.rb
@@ -22,9 +22,26 @@ apt_package "hhvm" do
 	action :install
 end
 
+# Make hhvm run instead of php
 if node["phptools"]["hhvm"]["replaces_php"]
     # create a shortcut so every .phar file uses hhvm
     link "/usr/local/bin/php" do
         to "/usr/bin/hhvm"
     end
+end
+
+# Overwrite php.ini for hhvm
+cookbook_file "/etc/hhvm/php.ini" do
+  source "hhvm/php.ini"
+  mode 0664
+  owner "root"
+  group "root"
+end
+
+# Overwrite config.hdf for hhvm
+cookbook_file "/etc/hhvm/config.hdf" do
+  source "hhvm/config.hdf"
+  mode 0664
+  owner "root"
+  group "root"
 end
